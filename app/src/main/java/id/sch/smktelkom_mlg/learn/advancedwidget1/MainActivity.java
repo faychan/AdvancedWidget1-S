b69cffdca9984a97141f4bbfecc6c377faacf04a
Faychan <nor.iffat32@gmail.com>
2016-10-06 07:47:44 +0700

    -Initial Commit -Basic Butter Knife Final -Widget Programming Final Part

--- app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget1/MainActivity.java
@@ -12,26 +12,26 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Spinner spNummer;
+    Spinner spAnzahl;
     LinearLayout llKinder;
-    TextView tvHasil;
+    TextView tvErgebnis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spNummer = (Spinner) findViewById(R.id.spinnerNummerKinder);
-        Integer[] arNummer = new Integer[10];
+        spAnzahl = (Spinner) findViewById(R.id.spinnerAnzahlKinder);
+        Integer[] arAnzahl = new Integer[10];
         for (int i = 0; i < 10; i++) {
-            arNummer[i] = i + 1;
+            arAnzahl[i] = i + 1;
         }
-        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, arNummer);
+        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, arAnzahl);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spNummer.setAdapter(adapter);
+        spAnzahl.setAdapter(adapter);
 
         llKinder = (LinearLayout) findViewById(R.id.linearLayoutKinder);
-        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        tvErgebnis = (TextView) findViewById(R.id.textViewErgebnis);
 
         findViewById(R.id.buttonProses).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,10 +40,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        spNummer.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        spAnzahl.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int i, long l) {
-                addEditText((int) spNummer.getSelectedItem());
+                addEditText((int) spAnzahl.getSelectedItem());
             }
 
             @Override
@@ -53,9 +53,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void addEditText(int jumlah) {
+    private void addEditText(int anzahl) {
         llKinder.removeAllViews();
-        for (int i = 1; i <= jumlah; i++) {
+        for (int i = 1; i <= anzahl; i++) {
             View v = LayoutInflater.from(this).inflate(R.layout.layout_anak, llKinder, false);
             v.setTag("Kinder" + i);
             llKinder.addView(v);
@@ -63,23 +63,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void doProses() {
-        int jumlah = (int) spNummer.getSelectedItem();
+        int anzahl = (int) spAnzahl.getSelectedItem();
         String hasil = "";
-        for (int i = 1; i <= jumlah; i++) {
+        for (int i = 1; i <= anzahl; i++) {
             LinearLayout llNow = (LinearLayout) llKinder.findViewWithTag("Kinder" + i);
 
-            EditText etNama = (EditText) llNow.findViewById(R.id.editTextNama);
-            EditText etUmur = (EditText) llNow.findViewById(R.id.editTextUmur);
+            EditText etName = (EditText) llNow.findViewById(R.id.editTextName);
+            EditText etAlter = (EditText) llNow.findViewById(R.id.editTextAlter);
 
-            String nama = etNama.getText().toString().trim();
-            String umur = etUmur.getText().toString();
+            String name = etName.getText().toString().trim();
+            String alter = etAlter.getText().toString();
 
-            if (umur.isEmpty())
-                umur = "0";
-            if (!nama.isEmpty())
-                hasil += "Kinder ke-" + i + ": " + nama + " umur " + umur + " tahun\n";
+            if (alter.isEmpty())
+                alter = "0";
+            if (!name.isEmpty())
+                hasil += "Kinder-" + i + ": " + name + alter + " Jahre alt\n";
         }
-        tvHasil.setText(hasil);
+        tvErgebnis.setText(hasil);
     }
 
 }
